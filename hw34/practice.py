91c9c3603687b3931c83c5f9fe87267adf8d27f4
woof-ПК\woof <alegthunder@gmail.com>
2017-09-15 00:09:27 +0300

    upd

--- hw34/practice.py
+++ hw34/practice.py
@@ -1,6 +1,7 @@
-import pprint
+
 from urllib.parse import urlencode
 import requests
+from Tools.i18n.makelocalealias import pprint
 
 URL = 'https://oauth.vk.com/authorize'
 ID_APP = '6184907'
@@ -15,7 +16,7 @@ auth_data = {
 }
 
 token_url = '?'.join((URL, urlencode(auth_data)))
-print(token_url)
+# print(token_url)
 
 access_token = 'cc8aa3138445ca6e459cd794b6c95f7bf78f0e661203073fd3cef5b27f3ab940b676fd8482d744915ef25'
 
@@ -23,5 +24,5 @@ params = {
     'access_token': access_token,
     'v': '5.68'
 }
-response = requests.get('https://api.vk.com/method.status.get', params)
-print(response)
+response = requests.get('https://api.vk.com/method/friends.get', params)
+pprint(response.json()['items'])
